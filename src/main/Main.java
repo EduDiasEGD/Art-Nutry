ee23635dd42e0a350e85720d2bd9a4550da2072a
Eduardo Guilherme Dias <eduardo.imersaodev2021@gmail.com>
2022-11-26 14:36:17 -0300

    primeiro commit

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import View.Home_GUI;
+import View.Mercado_GUI;
+import View.TelaLogin_GUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new TelaLogin_GUI().setVisible(true);
+        //new Home_GUI().setVisible(true);
+        //new Mercado_GUI().setVisible(true);
+    }
+    
+}
